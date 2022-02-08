1f88314e5792d07baf490c2fdd95bc676e54f7e4
Mubor <muhatasov22@ukr.net>
2022-02-08 13:31:27 +0200

    destructuring added

--- src/lib/output.js
+++ src/lib/output.js
@@ -2,19 +2,20 @@
 const fill = (id, text) => document.getElementById(id).innerHTML = text;
 
 const output = (data) => {
-  const region = `${data.location.country}, ${data.location.region}, ${data.location.name}`;
-  const img = `<img src="https:${data.current.condition.icon}" alt="cloud">`;
-  const windSpeed = `${data.current.wind_kph} kph`;
-  const humidity = `${data.current.humidity}%`;
+  const { country, region, name, localtime} = data.location;
+  const { wind_kph, condition, humidity, temp_c, feelslike_c, pressure_mb} = data.current;
+  const reg = `${country}, ${region}, ${name}`;
+  const img = `<img src="https:${condition.icon}" alt="cloud">`;
+  const windSpeed = `${wind_kph} kph`;
 
-  fill('geoinfo', region);
-  fill('date', data.location.localtime);
-  fill('temp', data.current.temp_c);
+  fill('geoinfo', reg);
+  fill('date', localtime);
+  fill('temp', temp_c);
   fill('cloud-img', img);
-  fill('cloudiness', data.current.condition.text);
-  fill('feels', data.current.feelslike_c);
+  fill('cloudiness', condition.text);
+  fill('feels', feelslike_c);
   fill('wind', windSpeed);
-  fill('pressure', data.current.pressure_mb);
+  fill('pressure', pressure_mb);
   fill('humidity', humidity);
 };
 
